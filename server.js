a1527db9155686c1568f70b66365e913f5a14ab2
Perera K C G S <it21810664@my.sliit.lk>
2024-03-18 16:05:07 +0530

    adding dbConnect file, editing server.js and adding .env file and setting up the mongoDB connection

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const http = require("http");
+require("./config/dbConnect")
 const app = require("./app/app")
 
 const PORT = process.env.PORT || 2020
@@ -6,3 +8,8 @@ const PORT = process.env.PORT || 2020
 // server
 const server = http.createServer(app);
 server.listen(PORT, console.log(`Server is up and running on port ${PORT}`));
+
+
+
+// MongoDB Username - Gavithra
+// MongoDB Password - GaviMongo
