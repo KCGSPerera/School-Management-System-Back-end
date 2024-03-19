fcd62d8977cdc7d15fb6d26bef32fcc504753feb
Perera K C G S <it21810664@my.sliit.lk>
2024-03-19 14:49:54 +0530

    adding globalErrHandler.js to move the error checking logic there. creating the isLogin.js file

--- app/app.js
+++ app/app.js
@@ -1,32 +1,21 @@
 const express  = require('express');
 const morgan = require('morgan');
 const adminRouter = require('../routes/staff/adminRouter');
-
+const {globalErrHandler, notFoundErr,} = require('../middlewares/globalErrHandler');
+//const notFoundErr = require('../middlewares/globalErrHandler');
 const app = express();
 
 // Middlewares
 app.use(morgan("dev"));
 app.use(express.json());  // pass incoming JSON data 
 
-// Error handelling middleware
-app.use((err, req, res, next) => {
-    // status
-    // message
-    // stack
-    const stack = err.stack;
-    const message = err.message;
-    const status = err.status ? err.status : "failed";
-    const statusCode = err.statusCode ? err.statusCode : 500;
-    res.status(statusCode).json({
-        status, 
-        message,
-        stack,
-    });
-});
-
 // Routes
 
 // Admin 
 app.use("/api/v1/admins", adminRouter);
+
+// Error handelling middleware
+app.use(notFoundErr)
+app.use(globalErrHandler);
 
 module.exports = app;
