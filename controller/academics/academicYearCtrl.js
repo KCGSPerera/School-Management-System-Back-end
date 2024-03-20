b121881bbe4945a6fbe9397c2f60d7e4b1b4422c
Perera K C G S <it21810664@my.sliit.lk>
2024-03-20 07:36:46 +0530

    implement adding academic year logic inside the academicYearCtrl.js

--- controller/academics/academicYearCtrl.js
+++ controller/academics/academicYearCtrl.js
@@ -3,10 +3,24 @@ const AcademicYear = require("../../model/Academic/AcademicYear");
 const Admin = require("../../model/Staff/Admin");
 
 exports.createAcademicYear = AsyncHandler(async (req, res) => {
-    const { name, fromYear, toYear, createdBy } = req.body;
+    const { name, fromYear, toYear } = req.body;
+
+    // check if exist
+    const academicYear = await AcademicYear.findOne({name});
+    if(academicYear){
+        throw new Error("Academic year already exists");
+    }
+
+    // create academic year
+    const academicYearCreated = await AcademicYear.create({
+        name,
+        fromYear,
+        toYear,
+        createdBy: req.userAuth._id,
+    });
     res.status(201).json({
         status: "success",
         message: "Academic Year created successfully",
-        data: "",
+        data: academicYearCreated,
     })
 }); 
