05e7e7a712986826621682803fe65e57771e5b84
Perera K C G S <it21810664@my.sliit.lk>
2024-03-22 00:25:37 +0530

    adding the create subject funtion to the controller

---
+++ controller/academics/subjectCtrl.js
@@ -0,0 +1,105 @@
+const AsyncHandler = require("express-async-handler");
+const Admin = require("../../model/Staff/Admin");
+const { throws } = require("assert");
+const Subject = require("../../model/Academic/Subject");
+
+// @dec Create Subject
+// @route POST /api/v1/subjects
+// @access Private
+exports.createSubject = AsyncHandler(async (req, res) => {
+    const { name, description, academicTerm} = req.body;
+
+    // check if exist
+    const subject = await Program.findOne({name});
+    if(subject){
+        throw new Error("Subject already exists");
+    }
+
+    // create Subject
+    const subjectCreated = await Subject.create({
+        name,
+        description,
+        academicTerm,
+        createdBy: req.userAuth._id,
+    });
+    // push Subject into admin
+    const admin = await Admin.findById(req.userAuth._id);
+    admin.Subject.push(subjectCreated._id);
+    await admin.save();
+    
+    res.status(201).json({
+        status: "success",
+        message: "Subject created successfully",
+        data: subjectCreated,
+    })
+}); 
+
+// @dec Get all Program
+// @route GET /api/v1/programs
+// @access Private
+exports.getPrograms = AsyncHandler(async (req, res) => {
+    const programs = await Program.find();
+
+    res.status(201).json({
+        status: "success",
+        message: "Programs fetched successfully",
+        data: programs,
+    });
+}); 
+
+// @dec Get single program
+// @route GET /api/v1/programs/:id
+// @access Private
+exports.getProgram = AsyncHandler(async (req, res) => {
+    const program = await Program.findById(req.params.id);
+
+    res.status(201).json({
+        status: "success",
+        message: "Specified Program fetched successfully",
+        data: program,
+    });
+}); 
+
+// @dec Update program
+// @route PUT /api/v1/programs/:id
+// @access Private
+exports.updateProgram = AsyncHandler(async (req, res) => {
+    const { name, description } = req.body;
+
+    // check pogram exists
+    const createProgramFound = await Program.findOne({name});
+    if(createProgramFound){
+        throw new Error("Program already exists");
+    }
+
+    const program = await Program.findByIdAndUpdate(
+        req.params.id,
+        {
+            name, 
+            description,
+            createdBy: req.userAuth._id,
+        },
+        {
+            new : true,
+        }
+        );
+
+    res.status(201).json({
+        status: "success",
+        message: "Specified Class Level Updated successfully",
+        data: program,
+    });
+}); 
+
+// @dec Delete Program
+// @route DELETE /api/v1/programs/:id
+// @access Private
+exports.deleteProgram = AsyncHandler(async (req, res) => {
+    
+    await Program.findByIdAndDelete(req.params.id);
+
+    res.status(201).json({
+        status: "success",
+        message: "Specified Program Deleted successfully",
+    });
+}); 
