f4e148fd531ce0186a15c119145fdb38fb23c918
Perera K C G S <it21810664@my.sliit.lk>
2024-03-22 00:41:42 +0530

    adding subject routes to the subject.js file

---
+++ routes/academics/subject.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const isAdmin = require('../../middlewares/isAdmin');
+const isLogin = require('../../middlewares/isLogin');
+const { createSubject, getSubjects, updateSubject, deleteSubject } = require('../../controller/academics/subjectCtrl');
+    
+const subjectRouter = express.Router();
+
+
+// subjectRouter.post("/", isLogin, isAdmin, createAcademicYear);
+// subjectRouter.get("/", isLogin, isAdmin, getAcademicYears);
+
+// route chaining
+subjectRouter
+.route("/")
+.post(isLogin, isAdmin, createSubject)
+.get(isLogin, getSubjects);
+
+// subjectRouter.get("/:id", isLogin, isAdmin, getAcademicYear);
+// subjectRouter.put("/:id", isLogin, isAdmin, updateAcademicYear);
+// subjectRouter.delete("/:id", isLogin, isAdmin, deleteAcademicYear);
+
+subjectRouter
+.route("/:id")
+.get(isLogin, getSubject)
+.put(isLogin, isAdmin, updateSubject)
+.delete(isLogin, isAdmin, deleteSubject);
+
+module.exports = subjectRouter;
